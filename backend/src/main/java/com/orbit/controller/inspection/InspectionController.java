9d08d8135629c325e1db480925a2a630dc5852d4
ParkSeulKi23 <tmf296@naver.com>
2025-03-06 17:43:59 +0900

    feat: Invoice, Payment, Inspection 엔티티, 서비스, 컨트롤러 추가

--- backend/src/main/java/com/orbit/controller/inspection/InspectionController.java
+++ backend/src/main/java/com/orbit/controller/inspection/InspectionController.java
@@ -1,27 +1,12 @@
 package com.orbit.controller.inspection;
 
-import com.orbit.dto.inspection.InspectionRequestDto;
-import com.orbit.dto.inspection.InspectionResponseDto;
+import com.orbit.entity.inspection.Inspection;
 import com.orbit.service.inspection.InspectionService;
-import org.springframework.http.ResponseEntity;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-
 
 import java.util.List;
 import java.util.Optional;
-
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.orbit.entity.inspection.Inspection;
-import com.orbit.service.inspection.InspectionService;
-
-import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/inspections")
