cf048e20161aa4b86411dcd91cc4466c2c95bf1a
ParkSeulKi23 <tmf296@naver.com>
2025-03-07 10:43:41 +0900

    .

--- backend/src/main/java/com/orbit/repository/inspection/InspectionRepository.java
+++ backend/src/main/java/com/orbit/repository/inspection/InspectionRepository.java
@@ -4,5 +4,9 @@ import com.orbit.entity.inspection.Inspection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface InspectionRepository extends JpaRepository<Inspection, Long> {}
+public interface InspectionRepository extends JpaRepository<Inspection, Long> {
+    List<Inspection> findByInspectorId(Long inspectorId);
+}
