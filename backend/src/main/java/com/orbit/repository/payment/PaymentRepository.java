78c6e63311c280ca27d8fef2c8722d3ac1286063
ParkSeulKi23 <tmf296@naver.com>
2025-03-09 19:43:44 +0900

    wip: 검수, 인보이스, 결제 관련 파일 추가,삭제 (미완성)

--- backend/src/main/java/com/orbit/repository/payment/PaymentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.orbit.repository.payment;
-
-import com.orbit.entity.payment.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PaymentRepository extends JpaRepository<Payment, Long> {}
