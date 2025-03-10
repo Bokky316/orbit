cbd02f39b005c062631abc9462d36a4125368d95
Bokky <Bokky316@gmail.com>
2025-03-10 17:53:29 +0900

    feat: 파일 다운로드 가능

--- backend/src/main/java/com/orbit/entity/procurement/PurchaseRequestAttachment.java
+++ backend/src/main/java/com/orbit/entity/procurement/PurchaseRequestAttachment.java
@@ -1,14 +1,17 @@
 package com.orbit.entity.procurement;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Getter
 @Setter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // 기본 생성자 추가 (필수)
+@AllArgsConstructor // 전체 필드 생성자 추가 (선택)
 public class PurchaseRequestAttachment {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String fileName;
