9ff58fc1418aa8a5c6bf75aada3eeaa78993cae2
ParkSeulKi23 <tmf296@naver.com>
2025-03-10 10:01:36 +0900

    feat: 인보이스 관련 페이지 추가(미완성)
    - 인보이스 생성 페이지 (InvoiceCreatePage.jsx) 추가
    - 인보이스 상세 페이지 (InvoiceDetailPage.jsx) 추가
    - 인보이스 수정 페이지 (InvoiceEditPage.jsx) 추가
    - 인보이스 목록 페이지 (InvoicesListPage.jsx) 추가
    - App.jsx에 인보이스 관련 라우트 추가

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -40,53 +40,85 @@ import ProjectCreatePage from "@/pages/procurement/ProjectCreatePage";
 import PurchaseRequestListPage from "@/pages/procurement/PurchaseRequestListPage";
 import PurchaseRequestDetailPage from "@/pages/procurement/PurchaseRequestDetailPage";
 import PurchaseRequestCreatePage from "@/pages/procurement/PurchaseRequestCreatePage";
-import ApprovalListPage from "@/pages/approval/ApprovalListPage";
-import ApprovalDetailPage from "@/pages/approval/ApprovalDetailPage";
-import ApprovalManagementPage from "@/pages/approval/ApprovalManagementPage"; // 추가
-import InspectionsListPage from "@/pages/inspection/InspectionsListPage";
-import InspectionDetailPage from "@/pages/inspection/InspectionDetailPage";
-import InspectionFormPage from "@/pages/inspection/InspectionFormPage";
-import InvoicesListPage from "@/pages/invoice/InvoicesListPage";
-import InvoiceCreatePage from "@/pages/invoice/InvoiceCreatePage";
-import PaymentListPage from "@/pages/payment/PaymentListPage";
-import PaymentProcessPage from "@/pages/payment/PaymentProcessPage";
-
-/**
- * AppContent 컴포넌트: 라우팅 설정 및 페이지 레이아웃 관리
- * @returns {JSX.Element} - 전체 앱 콘텐츠
- */
-/**
- * AppContent 컴포넌트: 라우팅 설정 및 페이지 레이아웃 관리
- * @returns {JSX.Element} - 전체 앱 콘텐츠
- */
+import ApprovalListPage from "@/pages/procurement/ApprovalListPage";
+import ApprovalDetailPage from "@/pages/procurement/ApprovalDetailPage";
+import InspectionsListPage from "@/pages/inspection/InspectionsListPage"
+import InspectionDetailPage from "@/pages/inspection/InspectionDetailPage"
+import InspectionFormPage from "@/pages/inspection/InspectionFormPage"
+import InvoicesListPage from "@/pages/invoice/InvoicesListPage"
+import InvoiceDetailPage from "@/pages/invoice/InvoiceDetailPage"
+import InvoiceEditPage from "@/pages/invoice/InvoiceEditPage"
 function AppContent() {
   const { isLoggedIn } = useSelector((state) => state.auth);
 
-    return (
-        <div className="App">
-{/*             <Header /> */}
-{/*             <Layout> */}
-                <Routes>
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/" element={<Home />} />
-                    <Route path="/biddings" element={<ProtectedRoute><BiddingListPage /></ProtectedRoute>} />
-                    <Route path="/biddings/new" element={<ProtectedRoute><BiddingFormPage mode="create" /></ProtectedRoute>} />
-                    <Route path="/biddings/edit/:id" element={<ProtectedRoute><BiddingFormPage mode="edit" /></ProtectedRoute>} />
-                    <Route path="/projects" element={<ProtectedRoute><ProjectListPage /></ProtectedRoute>} />
-                    <Route path="/projects/:id" element={<ProtectedRoute><ProjectDetailPage /></ProtectedRoute>} />
-                    <Route path="/purchase-requests" element={<ProtectedRoute><PurchaseRequestListPage /></ProtectedRoute>} />
-                    <Route path="/purchase-requests/:id" element={<ProtectedRoute><PurchaseRequestDetailPage /></ProtectedRoute>} />
-                    <Route path="/approvals" element={<ProtectedRoute><ApprovalListPage /></ProtectedRoute>} />
-                    <Route path="/approvals/:id" element={<ProtectedRoute><ApprovalDetailPage /></ProtectedRoute>} />
-                    <Route path="/inspections" element={<ProtectedRoute><InspectionsListPage /></ProtectedRoute>} />
-                    <Route path="/inspections/:id" element={<ProtectedRoute><InspectionDetailPage /></ProtectedRoute>} />
-                    <Route path="/inspections/:id/edit" element={<ProtectedRoute><InspectionFormPage /></ProtectedRoute>} />
-                    <Route path="*" element={<ErrorPage type="notFound" />} />
-                </Routes>
-{/*             </Layout> */}
-{/*             <Footer /> */}
-        </div>
-    );
+  // 대시보드 페이지 임시 컴포넌트
+  const DashboardPage = () => <div>대시보드 페이지</div>;
+
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          {isLoggedIn ? (
+            <Route element={<Home />}>
+              <Route path="/" element={<DashboardPage />} />
+              <Route path="/dashboard" element={<DashboardPage />} />
+
+              {/* 입찰 관리 */}
+              <Route path="/biddings" element={<BiddingListPage />} />
+              <Route
+                path="/biddings/new"
+                element={<BiddingFormPage mode="create" />}
+              />
+              <Route
+                path="/biddings/edit/:id"
+                element={<BiddingFormPage mode="edit" />}
+              />
+              <Route
+                path="/biddings/price-test"
+                element={<BiddingPriceTestPage />}
+              />
+
+              {/* 프로젝트 관리 */}
+              <Route path="/projects" element={<ProjectListPage />} />
+              <Route path="/projects/:id" element={<ProjectDetailPage />} />
+
+              {/* 구매 요청 관리 */}
+              <Route
+                path="/purchase-requests"
+                element={<PurchaseRequestListPage />}
+              />
+              <Route path="/purchase-requests/:id" element={<PurchaseRequestDetailPage />} />
+              <Route path="/purchase-requests/:id" element={<PurchaseRequestDetailPage />}/>
+              <Route path="/purchase-requests/new" element={<PurchaseRequestCreatePage />}/>
+
+              {/* 승인 관리 */}
+              <Route path="/approvals" element={<ApprovalListPage />} />
+              <Route path="/approvals/:id" element={<ApprovalDetailPage />} />
+
+              {/* 검수 관리 */}
+              <Route path="/inspections" element={<InspectionsListPage />} />
+              <Route path="/inspections/:id" element={<InspectionDetailPage />} />
+              <Route path="/inspections/:id/edit" element={<InspectionFormPage />} />
+
+              {/* 송장 관리 */}
+              <Route path="/invoices" element={<InvoicesListPage />} />
+              <Route path="/invoices/:id" element={<InvoiceDetailPage />} />
+              <Route path="/invoices/:id" element={<InvoiceDetailPage />} />
+              <Route path="/invoices/:id/edit" element={<InvoiceEditPage />} />
+
+              {/* 404 페이지 */}
+              <Route path="*" element={<ErrorPage type="notFound" />} />
+            </Route>
+          ) : (
+            <>
+              <Route path="/login" element={<Login />} />
+              <Route path="*" element={<Navigate to="/login" replace />} />
+            </>
+          )}
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 function App() {
