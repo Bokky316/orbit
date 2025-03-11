5126b2f479ef5f6cf07977c46605fce338df7fd4
Bokky <Bokky316@gmail.com>
2025-03-11 16:15:19 +0900

    멤버 재정리. 결재선 관련 정리

--- backend/src/main/java/com/orbit/dto/approval/ApprovalLineResponseDTO.java
+++ backend/src/main/java/com/orbit/dto/approval/ApprovalLineResponseDTO.java
@@ -1,27 +1,20 @@
 package com.orbit.dto.approval;
 
+import com.orbit.entity.approval.ApprovalLine;
+import lombok.Builder;
+import lombok.Data;
+
 import java.time.LocalDateTime;
 
-import com.orbit.entity.commonCode.ChildCode;
-import lombok.*;
-
-import java.time.LocalDateTime;
-
-@Getter
-@Setter
+@Data
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class ApprovalLineResponseDTO {
     private Long id;
-    private Long purchaseRequestId;
     private String approverName;
     private String department;
     private Integer step;
-
-    // ChildCode로 변경
-    private ChildCode status;
-
+    private ApprovalLine.ApprovalStatus status;
     private LocalDateTime approvedAt;
     private String comment;
 }
+
